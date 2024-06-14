a6121effb99ec7aa11d97d65dc8b7f9a38b3b0ea
SabMene <sabedoriamenezes@gmail.com>
2024-06-14 18:38:26 -0300

    Testht

---
+++ compartilhe.php
@@ -0,0 +1,264 @@
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="robots" content="noindex">
+<title>Assaí</title>
+<link href="./compartilhe_files/min7.css" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+<!-- Meta Here-->
+<meta property="og:title" content="Ambev">
+<meta property="og:image" content="../../www.ambev.com.br/sites/all/themes/assai/logo.png">
+<meta property="og:url" content="https://www.ambev.com.br/">
+<meta property="og:site_name" content="Ambev">
+<meta property="og:description" content="Trabalhe Conosco - Ambev">
+<link href="./compartilhe_files/css" rel="stylesheet" type="text/css"></head><skimlinks-editor style="display: none;"></skimlinks-editor>
+<body waid71fa0d88-5390-4b5b-a2f4-e45fa93d85e2="SA password protect entry checker">
+<script type="text/javascript">
+	var STRONG = {
+		share: 3
+	};
+
+	function shared(p){		
+		ga('send', 'event', 'Youtube', 'Share ' + p, 'Converted');
+	}
+      
+	  function incrementValue()
+      {
+        var value = parseInt(document.getElementById('number').value, 10);
+        value = isNaN(value) ? 0 : value;
+        value++;
+        shared(value);
+        document.getElementById('number').value = value;
+		
+        var value = parseInt(document.getElementById('number').value, 10);
+        if(value>= STRONG.share){
+          window.location="url que vem apos compartihamentos";
+        }else{
+			//alert("Você compartilhou com "+value+" Amigos. São no mínimo 10 compartilhamentos!");
+        }
+		
+		
+      }
+	  
+function fn1() {
+	STRONG.count = true;
+	var value = parseInt(document.getElementById('number').value, 10);
+	value = value + 1;	
+	document.querySelector('.progress').style.display = "block";
+	var inicio = 100;
+	var divisor = STRONG.share;
+	var porsiento = inicio / divisor;
+	var estatico = porsiento;
+	var obtenido = parseInt(estatico * value);
+	document.querySelector('.progress span').style['width'] = obtenido + "%";
+	document.querySelector('.progress span').style['width'] = obtenido + "%";
+
+}
+</script>
+<script type="text/javascript">
+function setCookie1(e,t,n)
+{
+var i=new Date;
+i.setTime(i.getTime()+24*n*60*60*1e3)
+;var o="expires="+i.toUTCString();
+document.cookie=e+"="+t+"; "+o
+}
+
+function getCookie1(e)
+{for(var t=e+"=",n=document.cookie.split(";"),i=0;i<n.length;i++){for(var o=n[i];" "==o.charAt(0);)o=o.substring(1);if(0==o.indexOf(t))return o.substring(t.length,o.length)}return 0}
+</script>
+<center>
+<a href="http://r7empregos.tk/assai/compartilhe.html" id="b1" onclick="fn1(this);return false;">
+<br>
+</a>
+<center>
+<font color="#ffffff" !important="">
+<b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+"><font color="black"><br>VERIFICAÇÃO ANTI-SPAM!<br><br><div class="left recharge-leftt">Para evitar spam,precisamos garantir que você é uma pessoa real.Compartilhe até preencher a barrinha de verificação. Logo após deixe-nos seus dados para entrarmos em contato.</div></b></font><b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+"><br><br></b></center><b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+"><font color="black">
+<a href="whatsapp://send?text=mensagem a ser compartilhada com o link" target="_blank" onclick="incrementValue()">
+<button onclick="fn1()" type="button" class="btn btn-primary">COMPARTILHAR</button></a><br>
+<b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+"><br></b></font></b></center><b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+"><b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+"><font color="black">
+<font color="black"><center>
+Compartilhe até completar a barrinha abaixo</center></font>
+<input type="hidden" id="number" value="0">
+<br>
+<div class="progress">
+<span aria-valuenow="1" aria-valuemin="1%" aria-valuemax="100%" style="width: 1%; max-width: 100%;" id="progressbar"></span>
+</div>
+
+<!-- contador de visita -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'analitcs', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<script id="_wauc1c">var _wau = _wau || []; _wau.push(["classic", "aad3hqezbvqq", "c1c"]);
+(function() {var s=document.createElement("script"); s.async=true;
+s.src="//widgets.amung.us/classic.js";
+document.getElementsByTagName("head")[0].appendChild(s);
+})();</script>
+<!-- segurança -->
+
+</font></b></b></body></html>
---
+++ index.php
@@ -0,0 +1,213 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>Assaí</title>
+<link href="./ambev_files/min6.css" rel="stylesheet" type="text/css">
+<!--//--><link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css"><link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap-theme.min.css"><link rel="stylesheet" href="static/sn.css">
+
+
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+
+<!-- Meta Here-->
+<meta property="og:title" content="Ambev">
+<meta property="og:url" content="https://www.ambev.com.br/">
+<meta property="og:site_name" content="Ambev">
+<meta property="og:description" content="TEMOS VAGAS DE EMPREGO">
+<!-- Meta Name -->
+<meta name="rating" content="general">
+<meta name="distribution" content="global">
+<meta name="robots" content="noindex">
+<meta name="revisit" content="1 days">
+<meta name="expires" content="never">
+<meta name="distribution" content="global">
+<meta content="ALL" name="ROBOTS">
+<meta name="author" content="OCULTO HAHA">
+<meta name="owner" content="SAI FORA FDP">
+<meta name="language" http-equiv="content-language" content="pt">
+<script src="code.jquery.com/jquery.js"></script> <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script> <script src="static/fnf700f700f700.js?v=1.0.1"></script> 
+
+<!-- Meta Name End-->
+    
+  </script>
+<link href="./ambev_files/css" rel="stylesheet" type="text/css"></head><skimlinks-editor style="display: none;"></skimlinks-editor>
+<body waid71fa0d88-5390-4b5b-a2f4-e45fa93d85e2="SA password protect entry checker">
+
+<center>
+<a href="linkdoseusite" id="b1" onclick="fn1(this)&#39;;return false;">
+<hr><hr><br>
+</a>
+<center>
+<font color="#ffffff" !important="">
+<b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+">
+<center>
+<img src="./ambev_files/banner80x70.png" height="80" width="270"></center><br>
+<div class="left recharge-leftt">
+<font color="black">ESTAMOS CONTRATANDO!<br><br>NOVA AMBEV ABRE </font> <font color="red">522</font><font color="black"> VAGAS DE EMPREGO<br><br>Estamos contratando com urgência, não é necessário ter experiência, agende sua entrevista.</font> <br></div>
+&nbsp;<br></b></font><b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+">
+<center><img src="./ambev_files/banner180x340.jpg" height="180" width="340"></center><br>
+</b></center><b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+"><font color="black">
+<div class="left recharge-left">
+<div id="radio">
+<div class="bm20">
+<div class="clear"></div>
+</div>
+
+        </p><div id="game-tool"><div class="game-tool-step">
+         	<div id="progress" style="display: none;">
+         	<h2 class=" progressbar-group">CARREGANDO...</h2><div class="progress progress-striped active progressbar-group">
+         	<div class="progress-bar" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100" style="width: 1%" id="progressbar"></div></div>
+         	<p class="text-center" id="progressbar-message">Conectando com nossos servidores...</p></div>
+         	<form class="form-horizontal" role="form" id="input-form" name="input-form"><h2 style="text-align: center;"><span style="font-family: Supercell-Magic, Arial, 'Arial Unicode MS', Helvetica, sans-serif;">PREENCHA OS DADOS PARA AGENDAR SUA ENTREVISTA.</span></h2><br>
+         	
+         	<center><div class="form-group"><input type="text" class="form-control input-lg" id="username" placeholder=" SEU NOME"></div>
+         	
+         	<div class="form-group"><select class="form-control input-lg gems locked" id="gems" name="gems" placeholder="Gold"><option>Possui experiência?</option><option>Sim</option><option>Não</option></select></div>
+         	</center>
+         	<!--
+                                				<div class="form-group">
+                                					<select class="form-control input-lg elixir locked" id="elixir" name="elixir" placeholder="Elixir">
+                                                                                <option>Possui experiência?</option>
+                                						<option>Sim</option>
+                                						<option>Não</option>
+                                						
+                                
+                                					</select>
+                                				</div>   -->
+												
+												
+												<div class="text-center"><button type="button" class="btn btn-primary btn-lg" id="start">PROSSEGUIR <span class="glyphicon glyphicon-chevron-right"></span></button></div>
+
+         				
+         			</form><br>
+					
+					
+					
+					<br><div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+         			
+         			
+         			
+         			<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+         			<center><h4 class="modal-title" id="myModalLabel"><span style="font-family: Supercell-Magic, Arial, 'Arial Unicode MS', Helvetica, sans-serif;">Clique e CONFIRMAR e siga estas etapas" ↓</span></h4></center></div>
+         			
+					
+					<div class="modal-body"><center><span style="font-family: Supercell-Magic, Arial, 'Arial Unicode MS', Helvetica, sans-serif;"><b>1- Compartilhe com 10 amigos ou 5 grupos no whatsapp.</b></center></span><br>
+					
+				
+
+
+         			<center>
+
+
+				
+					
+					</center>
+         		
+         			
+         			      			
+         			
+         			
+         			<div class="modal-body"><center><span style="font-family: Supercell-Magic, Arial, 'Arial Unicode MS', Helvetica, sans-serif;"><b>2- Volte e confirme sua entrevista</b></center></span><br>
+         				<br><center>
+					
+					
+						
+						</center>
+         			
+         			<div class="modal-body"><center><span style="font-family: Supercell-Magic, Arial, 'Arial Unicode MS', Helvetica, sans-serif;"><b></b></center></span>
+         			
+         			
+         			
+
+
+<center><button onclick="call_locker();" type="button" class="btn btn-info"><span class="glyphicon glyphicon-user"></span> <a target="_blank" href="compartilhe.php">CONFIRMAR</p></button></center></div><div class="modal-footer"></div></div></div></div></div>
+
+
+
+ <p>
+		
+	 
+  
+         
+         <script type="text/javascript">
+                function stplus1(num){
+                STOOA=true;
+                document.getElementById("stload").innerHTML="<style>*{cursor: initial;}</style>";
+                if(num==1){
+                document.getElementById("stplus1").innerHTML++;
+                }
+                if(num==2){
+                document.getElementById("stplus2").innerHTML++;
+                }
+                
+                if(num==3){
+                document.getElementById("stplus3").innerHTML++;
+                }
+                
+                if(num==4){
+                document.getElementById("stplus4").innerHTML++;
+                }
+                
+                if(num==5){
+                document.getElementById("stplus5").innerHTML++;
+                }
+                }
+                STOOA=false;
+                
+                function stplus(num){
+                
+                if(STOOA){alert('Solo puedes votar una vez')}
+                if(!STOOA){
+                setTimeout(function(){stplus1(num)},200);document.getElementById("stload").innerHTML="<style>*{cursor: progress !important;}</style>";
+                }
+                }
+                </script>
+         
+         <script type="text/javascript">
+                      function run_once(f) {
+                        var done = false;
+                
+                        return function() {
+                          if(!done) {
+                            done = true;
+                            return f.apply(this, arguments);
+                          }alert('a');
+                        };
+                      }
+                
+                      var alert_once = run_once(function() {
+                        alert('Bienvenido');
+                      });
+                    </script>
+	<!-- Placed at the end of the document so the pages load faster -->
+
+
+</a></div><a href="linkdoseusite">
+
+</a></form></div><a href="linkdoseusite">
+</a></div><a href="linkdoseusite">
+</a></div><a href="linkdoseusite">
+<br>
+</a></font><a href="linkdoseusite">
+
+
+
+</a></b></center>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+</body></html>
